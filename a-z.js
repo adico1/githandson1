da32e374080438eb105b4c41306c32500d00f3e7
adico1 <adico1@gmail.com>
2019-03-12 23:22:34 +0200

    add log letters spec

---
+++ a-z.js
@@ -0,0 +1,7 @@
+/**
+ * add mock log a..z
+ * 4 devs in project 1) af, 2) gl, 3) mr & 4) sz
+ * each responsible to log of a different segment of letters i.e. af is responsible of a..f
+ * each commit starts but adding a log to a letter and changing the mock to resemble the change
+ */
+
